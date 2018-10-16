8fcfe8b1f7f5c7ef8eb6f0b3f6c9867ea788b0dd
kodeFant <lars.lillo@gmail.com>
2018-10-16 12:58:54 +0200

    First Commit

---
+++ src/NavBar.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
+
+  import styles from './NavBar.module.css';
+
+export default class NavBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+  render() {
+    return (
+      <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand className={styles.brand} href="/">Potato Royale</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/booking">Book table</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="/find-us">Find Us</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Menu
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Starters
+                  </DropdownItem>
+                  <DropdownItem>
+                    Main Dishes
+                  </DropdownItem>
+                  <DropdownItem>
+                    Desserts
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
